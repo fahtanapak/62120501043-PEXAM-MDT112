2b919dd095b9a706fc4579608e3ddadebeff6c94
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-07-04 13:47:27 +0700

    3. Complete

--- 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
+++ 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
@@ -1,12 +1,47 @@
+int motorPin1 = 9;
+int motorPin2 = 10;
+int motorPin3 = 11;
+int motorPin4 = 12;
+int motorSpeed = 16; 
 void setup(){
-   pinMode(8,OUTPUT);
+   //pinMode(8,OUTPUT);
    //tone(8, 900);
    //delay(600);
    //noTone(8);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
    Serial.begin(9600);
-   Serial.println("Hello MDT");
+   //Serial.println("Hello MDT");
+   Serial.println("step CW 360 DEGREE");
+   delay(900);
+  // 1
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay(motorSpeed);
+  // 2
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+  delay (motorSpeed);
+  // 3
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(motorSpeed);
+  // 4
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(motorSpeed);
 }
 void loop()
 {  
-    
+  
 }
