640b9e6b70556a9aebfe65786b6f72353655aeb7
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-07-04 14:43:18 +0700

    5. Complete

--- 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
+++ 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
@@ -25,15 +25,12 @@ int motorPin3 = 11;
 int motorPin4 = 12;
 int motorSpeed = 16; 
 void setup(){
-   
- 
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
    Serial.println("step CW 360 DEGREE");
    delay(900);
- 
 }
 void loop()
 {  
@@ -63,24 +60,20 @@ void loop()
   delay(motorSpeed);
 
 }*/
+/* 
 int motorPin1 = 9;
 int motorPin2 = 10;
 int motorPin3 = 11;
 int motorPin4 = 12;
 int motorSpeed = 16; 
 void setup(){
-   
- 
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
    Serial.println("step CCW 360 DEGREE");
    delay(800);
- 
 }
-
-
 void loop()
 {   
    // 1
@@ -109,7 +102,18 @@ void loop()
   digitalWrite(motorPin3, LOW);
   digitalWrite(motorPin4, LOW);
   delay(motorSpeed);
- 
-  
+}*/
+#include "TM1637.h"
+const int CLK = 17;
+const int DIO = 16;
+TM1637 sevenSegment(CLK,DIO);
+void setup(){
+    sevenSegment.init(); 
+    sevenSegment.set(7);
+    sevenSegment.displayStr("0000");
+    delay(800);
+   sevenSegment.clearDisplay(); 
+}
+void loop(){
 
 }
