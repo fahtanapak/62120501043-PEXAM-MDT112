8ffd7667de59c3663f424fbe0bf8d65633467c97
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-07-04 14:12:52 +0700

    3. Complete

--- 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
+++ 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
@@ -1,22 +1,43 @@
+/* void setup(){
+   pinMode(8,OUTPUT);
+   tone(8, 900);
+   delay(600);
+   noTone(8); 
+}
+void loop()
+{
+
+}*/
+
+/* void setup(){
+    Serial.begin(9600);
+    Serial.println("Hello MDT");
+}
+void loop()
+{
+
+}*/
+
+
 int motorPin1 = 9;
 int motorPin2 = 10;
 int motorPin3 = 11;
 int motorPin4 = 12;
 int motorSpeed = 16; 
 void setup(){
-   //pinMode(8,OUTPUT);
-   //tone(8, 900);
-   //delay(600);
-   //noTone(8);
+   
+ 
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
-   Serial.begin(9600);
-   //Serial.println("Hello MDT");
    Serial.println("step CW 360 DEGREE");
    delay(900);
-  // 1
+ 
+}
+void loop()
+{  
+   // 1
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, LOW);
   digitalWrite(motorPin3, LOW);
@@ -40,8 +61,5 @@ void setup(){
   digitalWrite(motorPin3, LOW);
   digitalWrite(motorPin4, HIGH);
   delay(motorSpeed);
-}
-void loop()
-{  
-  
+
 }
