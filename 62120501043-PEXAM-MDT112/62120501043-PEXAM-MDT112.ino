7338791accd0fdfe61196bc495facffa46907e27
Tanapak Ngamsappakit <tanapakngamsuppaki@gmail.com>
2020-07-04 13:26:51 +0700

    2. Complete

--- 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
+++ 62120501043-PEXAM-MDT112/62120501043-PEXAM-MDT112.ino
@@ -1,9 +1,12 @@
 void setup(){
    pinMode(8,OUTPUT);
-   tone(8, 900);
-   delay(600);
-   noTone(8);
+   //tone(8, 900);
+   //delay(600);
+   //noTone(8);
+   Serial.begin(9600);
+   Serial.println("Hello MDT");
 }
 void loop()
 {  
+    
 }
